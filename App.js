6a48eaa75504f9ed87f4b8e488bbe0902ff8dada
Aman Mittal <amandeepmittal@live.com>
2024-02-12 00:42:49 +0530

    [dev] Update App.js

--- App.js
+++ App.js
@@ -117,7 +117,7 @@ export default function App() {
         <View style={styles.footerContainer}>
           <Button
             theme="primary"
-            label="Choose a photo"
+            label="Select a photo"
             onPress={pickImageAsync}
           />
           <Button
